42547b88cba8f6cf538830158696a362b1068f6d
RoGelione <2982237267@qq.com>
2017-11-10 19:46:43 +0800

    Update todolist.js

--- js/todolist.js
+++ js/todolist.js
@@ -1,46 +1,19 @@
-//var vm = new Vue({
-//	el:'#app',
-//	data:{
-//		task:{
-//			name:''
-//		},
-//		AllTask:[]//申明空数组。进行数据接收
-//	},
-//	mounted:function(){
-//		this.getData();
-//	},
-//	methods:{
-//		getData:function(){
-//			var self = this;
-//			this.$http.get("js/task-json.json").then(function (res){
-//				console.log(res);
-//				//获取当前数组的长度
-//				for(var i=0,len=res.body.lists.length;i<len;i++){
-//					//已经获取json数组中的数据，接下来如何传递到前端页面中
-//					//获取全部数据
-//					var selData = res.body.lists[i];
-//					//获取数组中的部分数据
-//					var part = res.body.lists[i].task;
-//					console.log(selData);
-//					//将获取的azx数据push到空的数组中itenList，
-//					self.AllTask.push(selData);
-//				}
-//			})
-//		},
-//		AddTask:function(){
-//			this.AllTask.push(this.task);
-//			this.task = {name:''}
-//		}
-//	}
-//});
-/*LocalStorage的写法，上面是用json数据预留，不好添加和修改*/
+/*
+ * taskTitle-->本地存储的任务数据
+ * CpTask-->前端view未完成任务数组
+ * OverTask-->前端view已完成数组
+ */
 var vm = new Vue({
 	el:'#app',
 	data:{
 		task:{
-			name:''
+			name:'',
+			cp:'false'
 		},
-		AllTask:[],//申明空数组。进行数据接收
+		check:true,
+		uncheck:false,
+		newname:'',
+		CpTask:[],//申明空数组。进行数据接收
 		OverTask:[]
 	},
 	mounted:function(){
@@ -48,118 +21,113 @@ var vm = new Vue({
 	},
 	methods:{
 		getLoc:function(){
-			var self = this;//上下问，用self来接收，可以防止下面出现匿名作用域
+			var self = this;
 //			var json_data ={"lists":[{"name":"吃饭"},{"name":"睡觉"},{"name":"打豆豆"}]};
 			if (window.localStorage) {
 				//从本地获取任务的json数据，需要转化才json数据类型
 				var json_data = '',
 					over_json_data = '';
-				/*判断是否有待完成的任务*/
+				/*判断是否有本地储存的任务数据*/
 				if (localStorage.getItem("taskTitle")) {
 					json_data = JSON.parse(localStorage.getItem("taskTitle"));
 					console.log("有存储的taskTitle数据")
 				}else{
 					console.log("没有存储的taskTitle数据")
 					/*没有数据则插入初始数据*/
-					json_data={"lists":[{"name":"吃饭"},{"name":"睡觉"},{"name":"打豆豆"}]};
+					json_data={"lists":[{"name":"吃饭","cp":'false'},{"name":"睡觉","cp":'false'},{"name":"打豆豆","cp":'false'}]};
 					localStorage.setItem("taskTitle", JSON.stringify(json_data));
 //					console.log("没有数据则插入初始数据");
 				}
-				/*判断是否有已完成的任务，没有则插入空数据*/
-				if (localStorage.getItem("OvertaskTitle")) {
-					over_json_data = JSON.parse(localStorage.getItem("OvertaskTitle"));
-					console.log("有存储的OvertaskTitle数据")
-					
-				}else{
-					console.log("没有存储的OvertaskTitle数据")
-					/*没有数据则插入初始数据*/
-					over_json_data={"lists":[]};
-					localStorage.setItem("OvertaskTitle", JSON.stringify(over_json_data));
-//					console.log("没有存储的OvertaskTitle数据");
-				}
 				/*与上面的方式不同，这里是直接定义的json数据，获取长度为json_data.lists.length*/
-				for(var i=0,len=json_data.lists.length;i<len;i++){
+				for(let i=0,len=json_data.lists.length;i<len;i++){
 					//获取之后添加到AllTask数组中，显示在前端
-					self.AllTask.push(json_data.lists[i]);
-				}
-				for(var j=0,leng=over_json_data.lists.length;j<leng;j++){
-					//获取之后添加到AllTask数组中，显示在前端
-					console.log("你看得到我嘛？我是列出已完成任务的过程")
-					self.OverTask.push(over_json_data.lists[j]);
+					if(json_data.lists[i].cp=='true'){
+						self.OverTask.push(json_data.lists[i]);
+					}else{
+						self.CpTask.push(json_data.lists[i]);
+					}
 				}
 			} else {
-			    alert("不支持本地存储")	
+			    alert("您的浏览器不支持本地存储~");	
 			}
 		},
-		AddTask:function(){
+		AddTask(){
 			var self = this;
+			var name = self.newname;
 			var json_data = JSON.parse(localStorage.getItem("taskTitle"));
-			console.log(self.task)
+			self.task.name = name;
 			json_data.lists.push(self.task);
 			/*存储在本地setItem，需要转化为字符串类型*/
 			localStorage.setItem("taskTitle", JSON.stringify(json_data));
-			self.AllTask.push(self.task);
-			self.task = {name:''}
+			self.CpTask.push(self.task);
+			self.newname='';
 		},
-		RemoveTask:function(index){
+		RemoveTask(e){
 			var self = this;
-			/*前端页面的移除，splice(下标位置，从这个位置起的个数)*/
-			self.AllTask.splice(index,1);
-			/*本地数据的移除，先从本地的数据取出数组删除，之后再添加，暂时想不到其他的办法*/
+			var save = e.target.offsetParent;
+			var index = save.id;//获取删除对象的
+			var cp = save.getAttribute("dcp");//获取删除对象的cp值，是否完成还是未完成
 			var json_data = JSON.parse(localStorage.getItem("taskTitle"));
-			json_data.lists.splice(index,1);
-			localStorage.setItem("taskTitle", JSON.stringify(json_data));
+			if(cp=='false'){
+				var name = self.CpTask[index].name;
+				self.CpTask.splice(index,1);/*前端页面的移除，splice(下标位置，从这个位置起的个数)*/
+				this.removet(name,json_data);/*本地数据的移除*/
+			}else{
+				var name = self.OverTask[index].name;
+				self.OverTask.splice(index,1);/*前端页面的移除，splice(下标位置，从这个位置起的个数)*/
+				this.removet(name,json_data);/*本地数据的移除*/
+			}
 		},
-		RemoveOverTask:function(index){
+		/*勾选任务-->未完成到已完成，或已完成到未完成*/
+		moveTask(e){
 			var self = this;
-			/*前端页面的移除，splice(下标位置，从这个位置起的个数)*/
-			self.OverTask.splice(index,1);
-			/*本地数据的移除，先从本地的数据取出数组删除，之后再添加，暂时想不到其他的办法*/
-			var over_json_data = JSON.parse(localStorage.getItem("OvertaskTitle"));
-			over_json_data.lists.splice(index,1);
-			localStorage.setItem("OvertaskTitle", JSON.stringify(over_json_data));
-		},
-		/*将未完成的任务勾选至已完成的列表中*/
-		toOverTask:function(index){
-			var self = this;
-			/*未完成前端页面的移除*/
-			self.AllTask.splice(index,1);
-			/*未完成的本地数据移除*/
+			var save = e.target.offsetParent;
+			var index = save.id;//获取删除对象的
+			var cp = save.getAttribute("dcp");//获取删除对象的cp值，是否完成还是未完成
+			var ncp =false;
 			var json_data = JSON.parse(localStorage.getItem("taskTitle"));
-			console.log(json_data.lists);
-			/*在移除前先取出*/
-			var tempTask = json_data.lists[index];
-			console.log("这里是移除未完成的任务的过程，index和数据是:,"+index+","+tempTask.name);
-			/*取出之后添加到已完成的前端页面，之后进行本地数据的添加*/
-			var over_json_data = JSON.parse(localStorage.getItem("OvertaskTitle"));
-			over_json_data.lists.push(tempTask);
-			self.OverTask.push(tempTask);
-			console.log(over_json_data);
-			localStorage.setItem("OvertaskTitle", JSON.stringify(over_json_data));
-			json_data.lists.splice(index,1);
-			localStorage.setItem("taskTitle", JSON.stringify(json_data));
-		},
-		toAllTask:function(index){
-			var self = this;
-			/*已完成的视图的移除*/
-			self.OverTask.splice(index,1);
-			/*已完成的本地数据移除*/
-			var over_json_data = JSON.parse(localStorage.getItem("OvertaskTitle"));
-			console.log(over_json_data);
-			/*在移除前先取出*/
-			var tempTask = over_json_data.lists[index];
-			console.log("这里是移除已完成的任务的过程，index和数据是:,"+index+","+tempTask.name);
-			/*取出之后添加到已完成的前端页面，之后进行本地数据的添加*/
-			var json_data = JSON.parse(localStorage.getItem("taskTitle"));//取出
-			json_data.lists.push(tempTask);
-			self.AllTask.push(tempTask);//前端页面显示
-			console.log(json_data);
-			/*添加后增加到本地数据*/
-			localStorage.setItem("taskTitle", JSON.stringify(json_data));
-			/*未完成任务的本地数据移除*/
-			over_json_data.lists.splice(index,1);
-			localStorage.setItem("OvertaskTitle", JSON.stringify(over_json_data));
+			if(cp=='false'){
+				let name = self.CpTask[index].name;
+				self.CpTask.splice(index,1);
+				let data={};
+				data.name = name;
+				data.cp = 'true';
+				self.OverTask.push(data);//有问题，待定
+				self.move(name,json_data,ncp);
+//				var ind = json_data.lists.indexOft(name);
+//				json_data.lists[ind].cp = !cp;
+			}else{
+				ncp = !ncp;
+				let name = self.OverTask[index].name;
+				self.OverTask.splice(index,1);
+				let data={};
+				data.name = name;
+				data.cp = 'false';
+				self.CpTask.push(data);//有问题，待定
+				self.move(name,json_data,ncp);
+//				var ind = json_data.lists.indexOft(name);
+//				json_data.lists[ind].cp = !cp;
+			}
 		}
-		
 	}
 });
+Vue.prototype.removet = function(val,jd){//删除函数
+	for (var i=0;i<jd.lists.length;i++) {
+		if(jd.lists[i].name == val){
+			jd.lists.splice(i,1);
+			localStorage.setItem("taskTitle", JSON.stringify(jd));
+		}
+	}
+}
+Vue.prototype.move = function(val,jd,cp){//移动函数
+	for (var i=0;i<jd.lists.length;i++) {
+		if(jd.lists[i].name == val){
+			if(cp){
+				jd.lists[i].cp = 'false';
+			}else{
+				jd.lists[i].cp = 'true';
+			}
+			localStorage.setItem("taskTitle", JSON.stringify(jd));
+		}
+	}
+}
